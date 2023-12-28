152440a197c1a61cbf13f3229018ad000c74f734
mindong2 <qlsnaos12@naver.com>
2023-12-28 21:44:46 +0900

    openai settings

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,25 @@
+// chat에 대한 기본 설정
+// https://sdk.vercel.ai/docs/guides/frameworks/nextjs-app
+
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+export const runtime = "edge";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY!,
+});
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages,
+  });
+
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream);
+}
---
+++ src/app/chat.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useChat } from "ai/react";
+
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <main>
+      <ul>
+        {messages.map((m, index) => (
+          <li key={index}>
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Say something...
+          <input className="border-cyan-600" value={input} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Send</button>
+      </form>
+    </main>
+  );
+}
